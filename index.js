82f6112649cb76bc7b6668570455ae6484546994
izzygren22 <igrenier22@penguinhall.org>
2019-12-11 12:40:21 -0500

    rename myFunctions to myPages

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ http.createServer(function (request, response) {
   if (filePath == './') {
     contentType = mimeTypes['.html'];
     response.writeHead(200, { 'Content-Type': contentType });
-    response.end(myFunctions.getPages(), 'utf-8');
+    response.end(myFunctions.getIndex(), 'utf-8');
   } else { // get a static file, like css, images, etc.
     fs.readFile(filePath, function (error, content) {
       if (error) {
--- myPages.js
+++ myPages.js
@@ -1,7 +1,30 @@
 module.exports = {
-    getPages
+    getIndex
   }
   
-  function getPages () {
-    return 'HELLO WORLD';
+  function getIndex () {
+    var msg = `
+<!DOCTYPE html>
+<html>
+<title>Snot Bot</title>
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+<meta charset="UTF-8">
+
+<div class="w3-row w3-container"> 
+  <div class="w3-fourth w3-container">
+    <h3>Logo Here</h3>
+  </div>
+  <div class="w3-twofourths w3-container">
+    <h1>Domain name here</h1>
+  </div>
+  <div class="w3-fourth">
+    <h2></h2>
+  </div>
+
+</div>
+
+<div class="w3-third w3-container w3-margin-bottom">
+  <h1>Filters</h1>
+</div> ` ;
+    return msg;
   }
