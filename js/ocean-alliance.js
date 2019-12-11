8e48b314fdc2c4bf657bdd2d518a50606502df4c
izzygren22 <igrenier22@penguinhall.org>
2019-12-11 13:32:05 -0500

    leafet fix

--- js/ocean-alliance.js
+++ js/ocean-alliance.js
@@ -1,8 +1,5 @@
 console.log('ocean-alliance.js working');
 //CODE!
-<script src="https://unpkg.com/leaflet@1.6.0/dist/leaflet.js"
-integrity="sha512-gZwIG9x3wUXg2hdXF6+rVkLF/0Vi9U8D2Ntg4Ga5I5BZpVkVxlJWbSQtXPSiUTtC0TjtGOmxa1AJPuV0CPthew=="
-crossorigin="">
 var map = L.map('map').setView([42.616131, -70.668930], 13);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
