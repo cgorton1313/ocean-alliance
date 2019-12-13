f789ac6d0255c2797286d5576b45e95e3bb2a7ef
Carling APH <carlingaph@Carlings-MacBook-Air.local>
2019-12-13 10:57:20 -0500

    update map

--- js/ocean-alliance.js
+++ js/ocean-alliance.js
@@ -1,11 +1,20 @@
 console.log('ocean-alliance.js working');
 //CODE!
-var map = L.map('map').setView([42.616131, -70.668930], 13);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+var greenIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+  var map = L.map('map').setView([43, -70], 8);
+
+        L.tileLayer(http://server.arcgisonline.com/ArcGIS/rest/services/Ocean_Basemap/MapServer/tile/{z}/{y}/{x}, {
         }).addTo(map);
 
-        L.marker([42.616131, -70.668930]).addTo(map)
-            .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-            .openPopup();
+        L.marker([42.616131, -70.668930]).addTo(map);
+
+        L.marker([42.605542, -70.664255], {icon: greenIcon}).addTo(map);
