bb753650e059a9ca25439b9f8b3be570bb9bf78f
Craig Gorton <cgorton@penguinhall.org>
2020-01-08 15:31:30 -0500

    getFlightData working

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const url = require('url');
 const myPages = require('./myPages.js');
 const config = require('./config.js');
 const snotbotData = require('./snotbotData.js')
@@ -29,39 +30,23 @@ http.createServer(function (request, response) {
   var filePath = '.' + request.url;
   var extname = String(path.extname(filePath)).toLowerCase();
   var contentType = mimeTypes[extname] || 'application/octet-stream';
+  const queryObject = url.parse(request.url, true).query;
 
   if (filePath == './') {
     myPages.getIndex(response);
   } else if (filePath.startsWith('./flights')) {    // return all flights (minimal data)
-      contentType = mimeTypes['.json'];
-      snotbotData.getFlights().then(function(flights) {
+    contentType = mimeTypes['.json'];
+    snotbotData.getFlights().then(function (flights) {
       response.writeHead(200, { 'Content-Type': contentType });
       response.end(JSON.stringify(flights), 'utf-8');
     });
-  }else if (filePath.startsWith('./flightData')) {    //return one flight (all the data)
-    let flightData = `
-      [
-      {
-      "flight": "10DR19_f2",
-      "takeoff_latitude": "19.15319",
-      "takeoff_longitude": -69.206062,
-      "date": "2019-03-01T05:00:00.000Z",
-      "country": "Dominican Republic",
-      "location": "Samana",
-      "waterbody": "Samana Bay",
-      "objective": "EBC",
-      "airframe": "Inspire 2",
-      "flight_start": "7:48:21 AM",
-      "flight_end": "7:51:51",
-      "flight_duration": "0:03:30",
-      "max_distance": "378",
-      "total_mileage": "678",
-      "common_name": "Humpback whale"
-      }
-      ]`
-      contentType = mimeTypes['.json'];
-      response.writeHead(200, {'Content-Type': contentType});
-      response.end(flightData, 'utf-8');
+  } else if (filePath.startsWith('./flightData')) {    //return one flight (all the data)
+    contentType = mimeTypes['.json'];
+    snotbotData.getFlightData(queryObject.flight).then(function (flightData) {
+      response.writeHead(200, { 'Content-Type': contentType });
+      response.end(JSON.stringify(flightData), 'utf-8'); console.log(flightData);
+    });
+
   } else { // get a static file, like css, images, etc.
     fs.readFile(filePath, function (error, content) {
       if (error) {
