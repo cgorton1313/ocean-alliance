c105cc1ad22a69323789018bac147e62e8f66e81
Craig Gorton <cgorton@penguinhall.org>
2020-01-09 15:43:09 -0500

    temp icons

---
+++ temp.js
@@ -0,0 +1,2 @@
+iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
