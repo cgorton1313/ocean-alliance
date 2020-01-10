4c9e5cd35a1bb47842e2dff15f2239239a93f821
Craig Gorton <cgorton@penguinhall.org>
2020-01-10 13:24:23 -0500

    delete temp.js

--- temp.js
+++
@@ -1,2 +0,0 @@
-iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
-shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
