7fa21ccbcc296678e513ecd1de31a47dacfec13f
izzygren22 <igrenier22@penguinhall.org>
2020-05-05 09:35:20 -0400

    all but flightData

--- index.js
+++ index.js
@@ -8,28 +8,20 @@ const snotbotData = require(__dirname + '/snotbotData.js');
 const log = require('simple-node-logger').createSimpleLogger('project.log');
 
 // send all the static stuff
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname + '/public')));
 
 //home page
-app.get('/', function(req,res){
+app.get('/', async function (req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
 //expedition
-app.get('/expeditions', function(req,res){
-  res.json(snotbotData.getExpedition());
+app.get('/expeditions', async function (req, res) {
+  res.json(await snotbotData.getExpeditions());
 });
 
+app.listen(config.app.port, logServer());
 
-app.listen(config.app.port, serverRunning() );
-
-function serverRunning() {
+function logServer() {
   log.info('Server running at http://127.0.0.1:' + config.app.port + '/');
 };
-
-
-
-// // send index.html
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname + '/index.html'));
-// });
