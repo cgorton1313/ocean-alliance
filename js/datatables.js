6e1550108716502af27fbe8f1a265f3e2c883c09
Craig Gorton <cgorton@penguinhall.org>
2020-05-07 07:55:48 -0400

    add note to Cat

--- js/datatables.js
+++ js/datatables.js
@@ -1,4 +1,7 @@
 console.log("we're here");
+
+// Cat - most of your new work will go here, to build a string that is html, add that using innerHTML =
+
 $(document).ready( function () {
     $('#flightsTable').DataTable();
     } );
