94ba881bdee5500535328e40579ce5bb660b53f9
Cat Martins <catmartins@MacBook-Pro.home>
2020-05-19 08:46:20 -0400

    final changes to table

--- js/datatables.js
+++ js/datatables.js
@@ -1,10 +1,5 @@
 getTableData();
 
-
-$(document).ready(function () {
-    $('#flightsTable').DataTable();
-});
-
 async function getTableData() {
     // fetch tabledata and turn into a let
     let response = await fetch('./dataTable');
@@ -34,22 +29,26 @@ let dataTableHTML = `
         <tbody> 
 `;
     for (let i = 0; i < dataTableStr.length; i++) {
-        dataTableHTML += "<tr><td>" + dataTableStr[i].expedition_name + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].expedition_location + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].flight + "</td></tr>";        
-        dataTableHTML += "<tr><td>" + dataTableStr[i].flight_date + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].flight_waterbody + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].objective + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].species + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].common_name + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].flight + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].flight_duration + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].max_altitude + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].max_distance + "</td></tr>";
-        dataTableHTML += "<tr><td>" + dataTableStr[i].total_distance + "</td></tr>";
+        dataTableHTML += "<tr><td>" + dataTableStr[i].expedition_name + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].expedition_location + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].flight + "</td>";        
+        dataTableHTML += "<td>" + dataTableStr[i].flight_date + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].flight_waterbody + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].objective + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].species + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].common_name + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].flight + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].flight_duration + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].max_altitude + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].max_distance + "</td>";
+        dataTableHTML += "<td>" + dataTableStr[i].total_distance + "</td>";
 
     };
 dataTableHTML += '</tbody></table>';
 document.getElementById('dataTable').innerHTML = dataTableHTML;
+
+$(document).ready(function () {
+    $('#flightsTable').DataTable();
+});
 
 }
